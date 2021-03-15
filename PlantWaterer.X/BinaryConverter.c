22f524dabf023469528904a4bfc1a1fb190a30b3
Chris Ruff <Chrisruff@live.ca>
2021-03-15 18:03:03 -0300

    Start of binary converter
    No real code just a header and empty c file

---
+++ PlantWaterer.X/BinaryConverter.c
@@ -0,0 +1,17 @@
+/*
+ * File:   BinaryConverter.c
+ * Author: root
+ *
+ * Created on March 13, 2021, 10:07 PM
+ */
+
+#include <BinaryConverter.h>
+
+
+int toSevenSegment(int binary, int maxVal);
+{
+}
+int toLCD(int binary)
+{
+    
+}
